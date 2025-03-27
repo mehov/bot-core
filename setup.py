00427e3a6ad7b41eb51d5e90a7ac460d98854ad0
Alex <7813306+mehov@users.noreply.github.com>
2025-03-27 16:32:10 +0100

    Fix inconsistent name
    > expected 'bot-core', but metadata has 'src'
    > ERROR: Could not find a version that satisfies the requirement bot-core

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='src',
+    name='bot-core',
     version='1.0',
-    packages=find_packages(),
+    packages=find_packages(where='.'),
     description='Reusable classes for building Telegram bots in Python',
 )
