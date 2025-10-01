ffce6c5b29fa716ba124d6417fa7d8c3798a9a94
Alex <7813306+mehov@users.noreply.github.com>
2025-10-01 20:46:48 +0200

    Require requests

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(
     version='1.0',
     packages=find_packages(where='.'),
     description='Reusable classes for building Telegram bots in Python',
+    install_requires = [
+        "requests",
+    ]
 )
