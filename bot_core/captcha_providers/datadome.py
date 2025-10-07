b1859d2d4fd857abf934c4958482544163ad8898
Alex <7813306+mehov@users.noreply.github.com>
2025-10-07 12:01:21 +0200

    Use full Response in receive_captcha_response
    Allows setting additional headers in response, e.g. cookies

--- bot_core/captcha_providers/datadome.py
+++ bot_core/captcha_providers/datadome.py
@@ -1,4 +1,5 @@
 import json
+from flask import Response
 from bot_core.captcha_providers import BaseProvider
 from bot_core import Captcha
 from bot_core import Credentials
@@ -8,23 +9,23 @@ class datadome(BaseProvider):
 
     def receive_captcha_response(self, data):
         if not self.credentials_id:
-            return "Invalid credentials_id", 400
+            return Response('Invalid credentials_id', 400)
         try:
             data = json.loads(data)
         except json.JSONDecodeError:
-            return "Invalid JSON data", 400
+            return Response('Invalid JSON data', 400)
         if data['view'] == 'captcha':
             new_captcha = Captcha(captcha_key=self.captcha_key, user_id=self.user_id, credentials_id=self.credentials_id)
             challenge_id = new_captcha.challenge_id()
             error, challenge, metadata = Captcha.read_challenge(challenge_id)
             print(f'\n\nCaptcha.read_challenge({challenge_id})\nError: {error}\nChallenge: {challenge}\nMetadata:\n{json.dumps(metadata, indent=4)}')
             if error:
-                return error, 500
+                return Response(error, 500)
             # Trigger saving new captcha URL
             new_captcha.challenge(provider='datadome', captcha_url=data['url'], user_agent=metadata.get('user_agent'))
-            return data, 303
+            return Response(data, 303)
         obj = Credentials(self.user_id)
         all = obj.all()
         all[int(self.credentials_id)]['cookie'] = data['cookie']
         obj.set(all)
-        return data, 200
+        return Response(data, 200)
